dc0efa35c14ea2c8da38361001348d8f4398baa2
Darián <dariancapoteq@gmail.com>
2024-04-21 12:17:44 -0400

    create view for teams

---
+++ src/components/widget/teamCard.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import type { Team } from '@/types/team'
+
+/**
+ * Props
+ */
+defineProps<{ team: Team }>()
+
+/**
+ * Data
+ */
+const LINKS = [
+  { label: 'Jugadores', to: '/' },
+  { label: 'Calendario', to: '/' },
+  { label: 'Estadísticas', to: '/' }
+]
+</script>
+
+<template>
+  <div
+    class="p-4 max-w-sm w-full sm:max-w-md bg-white rounded-lg border border-gray-200 shadow sm:flex sm:flex-col sm:justify-between dark:border-gray-700 dark:bg-gray-800"
+  >
+    <div class="flex flex-col gap-y-2 w-full sm:flex-row">
+      <!-- Image and name -->
+      <div class="flex flex-row items-center py-2">
+        <img class="w-24 sm:w-36 md:w-28" :src="team.img_url" :alt="`${team.name} team`" />
+        <h3
+          class="text-3xl ml-4 font-bold tracking-tight break-all sm:hidden text-gray-900 dark:text-white"
+        >
+          {{ team.name }}
+        </h3>
+      </div>
+
+      <div class="tracking-tight py-2">
+        <!-- Name at sm and above -->
+        <h3
+          class="text-3xl mb-2 text-center font-bold tracking-tight break-all hidden sm:block text-gray-900 dark:text-white"
+        >
+          {{ team.name }}
+        </h3>
+
+        <!-- Description -->
+        <div class="px-4 space-y-1">
+          <p class="text-gray-700 dark:text-gray-400">Año de Fundado: {{ team.year_founder }}</p>
+          <p class="text-gray-700 dark:text-gray-400">País: {{ team.country }}</p>
+          <p class="text-gray-700 dark:text-gray-400">Estadio: {{ team.stadium }}</p>
+          <p class="text-gray-700 dark:text-gray-400">Capacidad: {{ team.capacity }}</p>
+        </div>
+
+        <!-- Links -->
+        <div class="mt-4 flex justify-center gap-x-4 sm:hidden">
+          <RouterLink v-for="(link, i) in LINKS" :key="`team-card-link-${i}`" :to="link.to">
+            <button type="button" class="font-medium text-link">
+              {{ link.label }}
+            </button>
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+
+    <!-- Links at sm and above -->
+    <div class="mt-2 justify-center gap-x-5 hidden sm:flex sm:flex-row">
+      <RouterLink v-for="(link, i) in LINKS" :key="`team-card-link-${i}`" :to="link.to">
+        <button type="button" class="font-medium text-link">
+          {{ link.label }}
+        </button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
---
+++ src/views/TeamsView.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import TeamCard from '@/components/widget/teamCard.vue'
+import type { Team } from '@/types/teams'
+
+const teams: Ref<Team[]> = ref([
+  {
+    name: 'Bahia',
+    img_url: 'https://media.api-sports.io/football/teams/118.png',
+    year_founder: 1938,
+    country: 'Brazil',
+    stadium: 'Arena Fonte Nova',
+    capacity: 56500
+  },
+  {
+    name: 'Internacional',
+    img_url: 'https://media.api-sports.io/football/teams/119.png',
+    year_founder: 1909,
+    country: 'Brazil',
+    stadium: 'Estádio José Pinheiro Borda',
+    capacity: 50128
+  },
+  {
+    name: 'Botafogo',
+    img_url: 'https://media.api-sports.io/football/teams/120.png',
+    year_founder: 1904,
+    country: 'Brazil',
+    stadium: 'Estádio Nilton Santos',
+    capacity: 46931
+  },
+  {
+    name: 'Palmeiras',
+    img_url: 'https://media.api-sports.io/football/teams/121.png',
+    year_founder: 1914,
+    country: 'Brazil',
+    stadium: 'Allianz Parque',
+    capacity: 43713
+  },
+  {
+    name: 'Fluminense',
+    img_url: 'https://media.api-sports.io/football/teams/124.png',
+    year_founder: 1902,
+    country: 'Brazil',
+    stadium: 'Estadio Jornalista Mário Filho (Maracanã)',
+    capacity: 78838
+  }
+])
+</script>
+
+<template>
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        Copa de Brazil - Temporada 2024
+      </h2>
+      <p class="text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+        El fútbol es parte integral de la cultura brasileña y la Copa Nacional es un reflejo
+        vibrante de esta pasión por el deporte rey!
+      </p>
+    </div>
+    <div class="grid grid-cols-1 gap-4 mb-6 md:grid-cols-2 xl:mb-16 xl:grid-cols-3">
+      <TeamCard v-for="(team, i) in teams" :key="i" :team="team" class="mx-auto" />
+    </div>
+  </div>
+</template>
