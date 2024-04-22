4811fe6c745238cb218f7ba01ddf8261d05610a6
Darián <dariancapoteq@gmail.com>
2024-04-22 12:10:59 -0400

    align layout in player view

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -104,107 +104,105 @@ function setSelectedSummary(league: League) {
 }
 </script>
 <template>
-  <div class="mx-auto max-w-screen-xl bg-gray-50 px-4 py-8 lg:px-6 lg:py-16">
-    <div class="flex flex-col justify-around gap-5 xl:flex-row xl:items-start">
-      <div class="flex flex-col gap-5 sm:flex-row sm:items-start">
-        <!-- Player information -->
-        <div
-          class="mx-auto w-full max-w-lg flex-1 space-y-4 rounded-b-lg rounded-t-3xl bg-white pb-2 dark:border-gray-700 dark:bg-gray-800"
-        >
-          <div class="h-40 rounded-b-xl rounded-t-3xl bg-blue-500">
-            <img
-              :src="player.image_url"
-              :alt="`profile image of ${player.name}`"
-              class="relative -bottom-16 mx-auto w-full max-w-36 rounded-full"
-            />
-          </div>
-
-          <div class="p-5 pt-12">
-            <h5 class="text-center text-xl font-medium text-gray-800 dark:text-white">
-              {{ player.name }}
-            </h5>
-
-            <ul role="list">
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Edad</p>
-                  </div>
-                  <div class="font-medium text-gray-800 dark:text-white">
-                    {{ player.age }}
-                  </div>
-                </div>
-              </li>
-
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Fecha de nacimiento</p>
-                  </div>
-                  <div class="font-medium text-gray-800 dark:text-white">
-                    {{ player.date_of_birth }}
-                  </div>
-                </div>
-              </li>
-
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Lugar de nacimiento</p>
-                  </div>
-                  <div class="text-base font-medium text-gray-800 dark:text-white">
-                    {{ player.place_of_birth }}
-                  </div>
-                </div>
-              </li>
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">País de nacimiento</p>
-                  </div>
-                  <div class="text-base font-medium text-gray-800 dark:text-white">
-                    {{ player.country_of_birth }}
-                  </div>
-                </div>
-              </li>
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Nacionalidad</p>
-                  </div>
-                  <div class="text-base font-medium text-gray-800 dark:text-white">
-                    {{ player.nationality }}
-                  </div>
-                </div>
-              </li>
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Altura</p>
-                  </div>
-                  <div class="font-medium text-gray-800 dark:text-white">
-                    {{ player.height }} cm
-                  </div>
-                </div>
-              </li>
-              <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                  <div class="ms-4 min-w-0 flex-1">
-                    <p class="truncate text-gray-500 dark:text-white">Peso</p>
-                  </div>
-                  <div class="text-base font-medium text-gray-800 dark:text-white">
-                    {{ player.weight }} kg
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
+  <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+    <div class="flex flex-col justify-around gap-5 xl:grid xl:grid-cols-5">
+      <!-- Player information -->
+      <div
+        class="mx-auto w-full max-w-lg flex-1 space-y-4 rounded-b-lg rounded-t-3xl bg-white p-2 dark:border-gray-700 dark:bg-gray-800 xl:col-span-2"
+      >
+        <div class="h-40 rounded-b-xl rounded-t-3xl bg-blue-500">
+          <img
+            :src="player.image_url"
+            :alt="`profile image of ${player.name}`"
+            class="relative -bottom-16 mx-auto w-full max-w-36 rounded-full"
+          />
         </div>
-        <!-- / Player information -->
 
+        <div class="p-5 pt-12">
+          <h5 class="text-center text-xl font-medium text-gray-800 dark:text-white">
+            {{ player.name }}
+          </h5>
+
+          <ul role="list">
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Edad</p>
+                </div>
+                <div class="font-medium text-gray-800 dark:text-white">
+                  {{ player.age }}
+                </div>
+              </div>
+            </li>
+
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Fecha de nacimiento</p>
+                </div>
+                <div class="font-medium text-gray-800 dark:text-white">
+                  {{ player.date_of_birth }}
+                </div>
+              </div>
+            </li>
+
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Lugar de nacimiento</p>
+                </div>
+                <div class="text-base font-medium text-gray-800 dark:text-white">
+                  {{ player.place_of_birth }}
+                </div>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">País de nacimiento</p>
+                </div>
+                <div class="text-base font-medium text-gray-800 dark:text-white">
+                  {{ player.country_of_birth }}
+                </div>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Nacionalidad</p>
+                </div>
+                <div class="text-base font-medium text-gray-800 dark:text-white">
+                  {{ player.nationality }}
+                </div>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Altura</p>
+                </div>
+                <div class="font-medium text-gray-800 dark:text-white">{{ player.height }} cm</div>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex items-center">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-gray-500 dark:text-white">Peso</p>
+                </div>
+                <div class="text-base font-medium text-gray-800 dark:text-white">
+                  {{ player.weight }} kg
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- / Player information -->
+
+      <div class="flex flex-col gap-5 md:w-full md:flex-row md:items-start xl:col-span-3">
         <!-- Leagues -->
         <div
-          class="mx-auto w-full max-w-lg rounded-lg bg-white p-4 dark:border-gray-700 dark:bg-gray-800 sm:p-6"
+          class="mx-auto w-full max-w-sm rounded-lg bg-white p-4 dark:border-gray-700 dark:bg-gray-800 sm:p-6"
         >
           <h5 class="mb-3 text-lg font-medium text-gray-800 dark:text-white">Ligas</h5>
 
@@ -226,98 +224,97 @@ function setSelectedSummary(league: League) {
           </ul>
         </div>
         <!-- / Leagues -->
-      </div>
 
-      <!-- League summary -->
-      <div class="rounded-lg">
-        <div
-          class="mx-auto flex w-fit flex-row items-center rounded-t-xl bg-blue-500 p-2 text-white"
-        >
-          <img
-            :src="selected_summary.league_image_url"
-            :alt="`${selected_summary.league} league`"
-            class="w-8"
-          />
-          <span class="ms-3 flex-1 whitespace-nowrap">{{ selected_summary.league }}</span>
+        <!-- League summary -->
+        <div class="flex-1 rounded-lg bg-white py-4">
+          <div class="mx-auto flex w-fit flex-row items-center gap-x-4">
+            <img
+              :src="selected_summary.league_image_url"
+              :alt="`${selected_summary.league} league`"
+              class="w-8"
+            />
+            <h5 class="text-lg font-medium text-gray-800 dark:text-white">
+              {{ selected_summary.league }}
+            </h5>
+          </div>
+
+          <div class="mb-8 grid grid-cols-2 dark:bg-gray-800">
+            <!-- Position -->
+            <div
+              class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Posición</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.position }}</p>
+            </div>
+            <!-- / Position  -->
+
+            <!-- Alignments -->
+            <div
+              class="flex flex-col items-center justify-center rounded-t-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Alineaciones</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.alignments }}</p>
+            </div>
+            <!-- / Alignments -->
+
+            <!-- Minutes -->
+            <div
+              class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Minutos</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.minutes }}</p>
+            </div>
+            <!-- / Minutes -->
+
+            <!-- Rating -->
+            <div
+              class="flex flex-col items-center justify-center bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Calificación</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.rating }}</p>
+            </div>
+            <!-- / Rating -->
+
+            <!-- Second block -->
+            <!-- Position -->
+            <div
+              class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Posición</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.position }}</p>
+            </div>
+            <!-- / Position  -->
+
+            <!-- Alignments -->
+            <div
+              class="flex flex-col items-center justify-center bg-white p-2 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Alineaciones</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.alignments }}</p>
+            </div>
+            <!-- / Alignments -->
+
+            <!-- Minutes -->
+            <div
+              class="flex flex-col items-center justify-center rounded-bl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Minutos</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.minutes }}</p>
+            </div>
+            <!-- / Minutes -->
+
+            <!-- Rating -->
+            <div
+              class="rounded-br-lgbg-white flex flex-col items-center justify-center rounded-t-none p-4 text-center dark:border-gray-700 dark:bg-gray-800"
+            >
+              <h3 class="font-medium text-gray-800 dark:text-white">Calificación</h3>
+              <p class="my-2 text-gray-500">{{ selected_summary.rating }}</p>
+            </div>
+            <!-- / Rating -->
+          </div>
         </div>
-
-        <div class="mb-8 grid grid-cols-2 bg-white py-4 dark:bg-gray-800">
-          <!-- Position -->
-          <div
-            class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Posición</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.position }}</p>
-          </div>
-          <!-- / Position  -->
-
-          <!-- Alignments -->
-          <div
-            class="flex flex-col items-center justify-center rounded-t-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Alineaciones</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.alignments }}</p>
-          </div>
-          <!-- / Alignments -->
-
-          <!-- Minutes -->
-          <div
-            class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Minutos</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.minutes }}</p>
-          </div>
-          <!-- / Minutes -->
-
-          <!-- Rating -->
-          <div
-            class="flex flex-col items-center justify-center bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Calificación</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.rating }}</p>
-          </div>
-          <!-- / Rating -->
-
-          <!-- Second block -->
-          <!-- Position -->
-          <div
-            class="flex flex-col items-center justify-center rounded-tl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Posición</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.position }}</p>
-          </div>
-          <!-- / Position  -->
-
-          <!-- Alignments -->
-          <div
-            class="flex flex-col items-center justify-center bg-white p-2 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Alineaciones</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.alignments }}</p>
-          </div>
-          <!-- / Alignments -->
-
-          <!-- Minutes -->
-          <div
-            class="flex flex-col items-center justify-center rounded-bl-lg bg-white p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Minutos</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.minutes }}</p>
-          </div>
-          <!-- / Minutes -->
-
-          <!-- Rating -->
-          <div
-            class="rounded-br-lgbg-white flex flex-col items-center justify-center rounded-t-none p-4 text-center dark:border-gray-700 dark:bg-gray-800"
-          >
-            <h3 class="font-medium text-gray-800 dark:text-white">Calificación</h3>
-            <p class="my-2 text-gray-500">{{ selected_summary.rating }}</p>
-          </div>
-          <!-- / Rating -->
-        </div>
+        <!-- League summary -->
       </div>
-
-      <!-- League summary -->
     </div>
   </div>
 </template>
