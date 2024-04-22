d719fb2df7849674b47df1f7cab1798ecc6be1b1
Darián <dariancapoteq@gmail.com>
2024-04-21 23:45:03 -0400

    add icon for mobile navigation

---
+++ src/components/icons/LayoutIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    class="h-5 w-5"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" />
+    <rect x="4" y="4" width="6" height="5" rx="2" />
+    <rect x="4" y="13" width="6" height="7" rx="2" />
+    <rect x="14" y="4" width="6" height="16" rx="2" />
+  </svg>
+</template>
