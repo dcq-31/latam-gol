e40a1a3fe3de477b9bb895cd9838bf131eb9815f
Darián <dariancapoteq@gmail.com>
2024-04-21 23:46:23 -0400

    create empty views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TeamsView from '@/views/TeamsView.vue'
 import PlayerView from '@/views/PlayerView.vue'
+import SearchView from '@/views/SearchView.vue'
+import LeaguesView from '@/views/LeaguesView.vue'
+import LiveMatchesView from '@/views/LiveMatchesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +23,21 @@ const router = createRouter({
       path: '/player',
       name: 'player',
       component: PlayerView
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: SearchView
+    },
+    {
+      path: '/leagues',
+      name: 'leagues',
+      component: LeaguesView
+    },
+    {
+      path: '/live-matches',
+      name: 'live-matches',
+      component: LiveMatchesView
     }
   ]
 })
